e2ec8675f50933c4a2d817ff3e2d196142f651ab
AustinG <austin.geisler@vtsd.com>
2024-09-24 12:17:44 -0400

    Wrote loops.py and count_up.py Challenges

---
+++ 1-1-4/count_up.py
@@ -0,0 +1,8 @@
+# Selected numbers for this example are 11 and 852
+#Challenge: Write a while loop that counts up to 852 but not over, by 11
+
+total = 0
+while total + 11 <= 852:
+    total = total + 11
+
+print(total)
