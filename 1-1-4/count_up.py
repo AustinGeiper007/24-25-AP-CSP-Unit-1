44b664669964c8159ab7f64507c981458092df5d
AustinG <austin.geisler@vtsd.com>
2024-09-25 11:06:11 -0400

    finished count_up.py

--- 1-1-4/count_up.py
+++ 1-1-4/count_up.py
@@ -1,9 +1,9 @@
 total = 0
 increment = int(input("Enter a increment to increase by: "))
 Goal = int(input("Enter a goal to increase to: "))
-while total + increment <= 852:
+while total + increment <= Goal:
     total += increment
 
 print("Your total is: " + str(total))
 print("Which is as high as you can go")
-print("counting by 11, without exceeding 852")
+print("counting by " + str(increment) + " without exceeding " + str(Goal))
