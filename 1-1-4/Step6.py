cefb0f63300137c2217984a0c2f2004f73411a3a
AustinG <austin.geisler@vtsd.com>
2024-09-25 11:24:48 -0400

    Format in Step6.py

--- 1-1-4/Step6.py
+++ 1-1-4/Step6.py
@@ -4,9 +4,9 @@ num = int(input("Enter Numerator (number 1): "))
 dem = int(input("Enter Denominator (number 2): "))
 # loop while the numbers are not divisible (the remainder is not 0)
 while (num%dem) != 0:
-# inform user of result
+    # inform user of result
     print("These 2 numbers are not divisible")
-# gather user input again
+    # gather user input again
     num = int(input("Enter Numerator (number 1): "))
     dem = int(input("Enter Denominator (number 2): "))
 # inform user of result
