968ddc36e60e2e35e7cb7f9a388e563f20713e8e
AustinG <austin.geisler@vtsd.com>
2024-09-25 11:21:03 -0400

    Made Step6.py from 1.1.4

---
+++ 1-1-4/Step6.py
@@ -0,0 +1,13 @@
+#   a114_divisible.py
+# get two numbers from user
+num = int(input("Enter Numerator (number 1): "))
+dem = int(input("Enter Denominator (number 2): "))
+# loop while the numbers are not divisible (the remainder is not 0)
+while (num%dem) != 0:
+# inform user of result
+    print("These 2 numbers are not divisible")
+# gather user input again
+    num = int(input("Enter Numerator (number 1): "))
+    dem = int(input("Enter Denominator (number 2): "))
+# inform user of result
+print("These 2 numbers are divisible!")
