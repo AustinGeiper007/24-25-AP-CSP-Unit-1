2b49ee95f22a5ca6b8479437fec0b5891e3f1d17
AustinG <austin.geisler@vtsd.com>
2024-09-26 13:32:41 -0400

    Step10.py made with prediction for step 11

---
+++ 1-1-4/Step10.py
@@ -0,0 +1,10 @@
+#   a114_nested_loops_1.py
+
+# Prediction: This will print i then the number the outer loop iterates,
+# then print the same thing but with j
+# and then repeat for each iteration of i
+for i in range(10):
+  print ("i: ", i)
+
+  for j in range(5):
+    print ("  j: ", j)
