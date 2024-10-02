78685cd69be958dae14c899e3782a82cc4c2e947
AustinG <austin.geisler@vtsd.com>
2024-10-02 13:30:44 -0400

    made how_to_swap.py

---
+++ 1-1-6/how_to_swap.py
@@ -0,0 +1,10 @@
+# This is a basic swap function!
+# assign start values (temp doesn't really matter
+#                       for this function lol)
+temp = 7
+x = 500
+y = 600
+
+temp = y
+y = x
+x = temp
