988c49f4db6f0341c46cf572dc920bf789096741
AustinG <austin.geisler@vtsd.com>
2024-10-02 13:19:32 -0400

    Step33_Fix.py minor remaning for consistency

--- 1-1-6/Step33_Fix.py
+++ 1-1-6/Step33_Fix.py
@@ -3,48 +3,48 @@
 import turtle as trtl
 
 # create ladybug head
-ladyBugPainter = trtl.Turtle()
-ladyBugPainter.pensize(40)
-ladyBugPainter.circle(5)
+ladybug = trtl.Turtle()
+ladybug.pensize(40)
+ladybug.circle(5)
 
 # and body
-ladyBugPainter.penup()
-ladyBugPainter.goto(0, -55)
-ladyBugPainter.color("red")
-ladyBugPainter.pendown()
-ladyBugPainter.pensize(40)
-ladyBugPainter.circle(20)
-ladyBugPainter.setheading(270)
-ladyBugPainter.color("black")
-ladyBugPainter.penup()
-ladyBugPainter.goto(0, 5)
-ladyBugPainter.pensize(2)
-ladyBugPainter.pendown()
-ladyBugPainter.forward(75)
+ladybug.penup()
+ladybug.goto(0, -55)
+ladybug.color("red")
+ladybug.pendown()
+ladybug.pensize(40)
+ladybug.circle(20)
+ladybug.setheading(270)
+ladybug.color("black")
+ladybug.penup()
+ladybug.goto(0, 5)
+ladybug.pensize(2)
+ladybug.pendown()
+ladybug.forward(75)
 
 # config dots
 num_dots = 1
 xpos = -20
 ypos = -55
-ladyBugPainter.pensize(10)
+ladybug.pensize(10)
 
 # draw two sets of dots
 while (num_dots <= 4 ):
-  ladyBugPainter.penup()
-  ladyBugPainter.goto(xpos, ypos)
-  ladyBugPainter.pendown()
-  ladyBugPainter.circle(3)
-  ladyBugPainter.penup()
-  ladyBugPainter.goto(xpos + 30, ypos + 15)
-  ladyBugPainter.pendown()
-  ladyBugPainter.circle(2)
+  ladybug.penup()
+  ladybug.goto(xpos, ypos)
+  ladybug.pendown()
+  ladybug.circle(3)
+  ladybug.penup()
+  ladybug.goto(xpos + 30, ypos + 15)
+  ladybug.pendown()
+  ladybug.circle(2)
 
   # position next dots
   ypos = ypos + 25
   xpos = xpos + 5
   num_dots = num_dots + 2
 
-ladyBugPainter.hideturtle()
+ladybug.hideturtle()
 
 wn = trtl.Screen()
 wn.mainloop()
