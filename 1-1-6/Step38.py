19968a5cf102aca13dfc58fe3addc124f9b07e82
AustinG <austin.geisler@vtsd.com>
2024-10-03 13:11:59 -0400

    Step38.py only has 2 legs

---
+++ 1-1-6/Step38.py
@@ -0,0 +1,45 @@
+import turtle as trtl
+spider = trtl.Turtle()
+
+#body
+spider.pensize(70)
+spider.color("black")
+spider.penup()
+spider.goto(0, 55)
+spider.pendown()
+spider.circle(35)
+
+#head
+spider.pensize(40)
+spider.penup()
+spider.goto(0, 0)
+spider.pendown()
+spider.circle(10)
+
+#legs
+spider.pensize(5)
+
+for num2 in range(2):
+    if num2%2 == 0:
+        z = 1
+    else:
+        z = -1
+    spider.setheading(15 * z)
+    spider.penup()
+    spider.goto(0, 55)
+    spider.pendown()
+    spider.circle(120, 120 * z, 4)
+    spider.penup()
+    spider.goto(0, 70)
+    spider.pendown()
+    spider.setheading(15 * z)
+    spider.circle(90, 120 * z, 4)
+
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
