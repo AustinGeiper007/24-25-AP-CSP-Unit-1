2cebe65d5e55b68626e2e666a034c324f0cb1af2
AustinG <austin.geisler@vtsd.com>
2024-10-07 11:46:08 -0400

    Step9.py made

--- 1-1-7/Step1.py
+++ 1-1-7/Step1.py
@@ -1,4 +1,10 @@
 new_list = ["dog", "cat", "mouse", "bird", "monkey"]
 
+print("Before Append")
 for id in range(5):
     print(new_list[id])
+
+print("After Append")
+new_list.append("lion")
+for id2 in range(5):
+    print(new_list[id2])
---
+++ 1-1-7/Step9.py
@@ -0,0 +1,36 @@
+#   a117_traversing_turtles.py
+#   Add code to make turtles move in a circle and change colors.
+import turtle as trtl
+
+# create an empty list of turtles
+my_turtles = []
+
+# use interesting shapes and colors
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+
+
+
+for s in turtle_shapes:
+  t = trtl.Turtle(shape=s)
+  t.color(turtle_colors.pop())
+  t.penup()
+  my_turtles.append(t)
+
+#Name variables startx and starty
+startx = 0
+starty = 0
+
+#Moves turtle up to the right and the down to the right
+for t in my_turtles:
+  t.goto(startx, starty)
+  t.pendown()
+  t.right(45)
+  t.forward(50)
+
+#Increase coords, to move next tutrle forward
+  startx = startx + 50
+  starty = starty + 50
+
+wn = trtl.Screen()
+wn.mainloop()
