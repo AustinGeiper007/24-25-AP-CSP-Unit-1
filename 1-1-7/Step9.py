25ce5ba2a1f2d63ed09d66fc5276721dfcce815f
AustinG <austin.geisler@vtsd.com>
2024-10-08 13:46:40 -0400

    Completed 1-1-7 Step 15 in the Step9.py

--- 1-1-7/Step9.py
+++ 1-1-7/Step9.py
@@ -9,30 +9,29 @@ my_turtles = []
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
-size = 0
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
   t.color(turtle_colors.pop())
-  t.pensize(size)
-  size += 1
   t.penup()
   my_turtles.append(t)
 
 #Name variables startx and starty
 startx = 0
 starty = 0
+ang = 90
 
 #Moves turtle up to the right and the down to the right
 for t in my_turtles:
   t.goto(startx, starty)
+  t.setheading(ang)
   t.pendown()
   t.right(45)
   t.forward(50)
+  startx = t.xcor()
+  starty = t.ycor()
+  ang = t.heading()
 
-#Increase coords, to move next tutrle forward
-  startx = startx + 50
-  starty = starty + 50
 
 wn = trtl.Screen()
 wn.mainloop()
