1f05b20a3c374341acb6e3abf783ddacd39da263
AustinG <austin.geisler@vtsd.com>
2024-10-10 12:12:20 -0400

    Completed Step12.py

---
+++ 1-1-8/Step12.py
@@ -0,0 +1,13 @@
+# Prediction:
+# Print a list of the days of the week,
+# and sub-lists under each day of the 3 courses
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+my_courses = ["English", "Math", "CS"]
+
+for d in days_of_week:
+  print(d)
+
+  for c in my_courses:
+    print("  " + c)
+
+# Prediction Status: Correct
