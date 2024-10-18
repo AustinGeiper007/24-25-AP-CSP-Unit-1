b01995dbfe8f8ae9392841bbbbb95cdd64da5cf6
AustinG <austin.geisler@vtsd.com>
2024-10-18 12:10:36 -0400

    In project.py, the sun, ground and sky are completed. Cloud is a work in progress and the house has not been started

--- 1-1-9/project.py
+++ 1-1-9/project.py
@@ -1,4 +1,5 @@
 import turtle as trtl
+import random as rd
 
 # sky code here (makes background with just a really big circle)
 # Finished
@@ -9,15 +10,43 @@ sky.pensize(5000)
 sky.circle(100)
 sky.hideturtle()
 
+# sun code
+# Finished
+sun = trtl.Turtle()
+sun.hideturtle()
+sun.speed(0)
+sun.pensize(75)
+sun.color('yellow')
+# Set up sun
+sun.penup()
+sun.goto(-400, 300)
+sun.pendown()
+sun.showturtle()
+sun.circle(5)
+# Rays
+sun.pensize(5)
+for ray in range(8):
+    sun.hideturtle()
+    sun.penup()
+    sun.goto(-400, 305)
+    sun.setheading((ray * 45) + 10)
+    sun.forward(60)
+    sun.pendown()
+    sun.showturtle()
+    sun.forward(30)
+sun.hideturtle()
+
 # ground code
 #Finished
 grass = trtl.Turtle()
+grass.hideturtle()
 grass.speed(0)
 grass.color('#3f9b0b')
 grass.pensize(250)
 grass.penup()
 grass.goto(-500, -350)
 grass.pendown()
+grass.showturtle()
 grass.forward(1000)
 grass.hideturtle()
 
@@ -29,15 +58,13 @@ cloud.penup()
 cloud.goto(400, 300)
 cloud.pendown()
 cloud.circle(5)
-
-
+cloud.penup()
+cloud.goto(360, 310)
+cloud.pendown()
+cloud.circle(5)
 
 # house code
 house = trtl.Turtle()
-
-# sun code
-sun = trtl.Turtle()
-
 
 
 
